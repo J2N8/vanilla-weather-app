430c9ffb095bf28dd47c8c39a68ab19c82678a61
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-09-10 16:45:43 -0400

    Added relevant icon from the api

--- src/script.js
+++ src/script.js
@@ -38,6 +38,13 @@ function showWeatherDescription(response) {
 
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchingCity(city) {
