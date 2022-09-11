6fdebb7c7f23a79b0173a283adc2f0fd28291391
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-09-10 21:19:13 -0400

    Updating the JS to get the current forecast of the coordinates/location

--- src/script.js
+++ src/script.js
@@ -23,15 +23,15 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  days.forEach(function(day) {
-  forecastHTML =
-    forecastHTML +
-    `
+  let days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
       <div class="days-col col">
         <div class="days">
           <p>
@@ -45,11 +45,16 @@ function displayForecast() {
         </div>
       </div>
     `;
-  })
-  
+  });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showWeatherDescription(response) {
@@ -72,6 +77,8 @@ function showWeatherDescription(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function searchingCity(city) {
@@ -134,4 +141,3 @@ let locationbutton = document.querySelector("#locator-button");
 locationbutton.addEventListener("click", getCurrentLocation);
 
 searchingCity("Tampa");
-displayForecast();
